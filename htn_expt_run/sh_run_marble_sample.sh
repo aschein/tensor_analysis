84b66b59d1e304bebd9fbe8b3b9d6fbe5dc6ed1d
Robert Chen <robchen401@gmail.com>
2014-09-02 20:46:21 -0400

    updated code for experiment runs -- sh scripts for qsub, etc

--- htn_expt_run/sh_run_marble_sample.sh
+++ htn_expt_run/sh_run_marble_sample.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
-python ../experiment_code/loadtensor_runfactorization_printphenotypes.py 50 1 0.001,0.1,0.1 /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_data_tensors/htn-tensor-subsetforanalysis-20140811-{0}.dat /nv/pcoc1/rchen87/tensor_factorization/github_tensor/experiment_code/ /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_expt_run/output_20140828/
+#python ../experiment_code/loadtensor_runfactorization_printphenotypes.py 50 1 0.001,0.02,0.02 /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_data_tensors/htn-tensor-subsetforanalysis-20140811-{0}.dat /nv/pcoc1/rchen87/tensor_factorization/github_tensor/experiment_code/ /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_expt_run/output_20140829/
 
+#python ../experiment_code/loadtensor_runfactorization_printphenotypes.py 50 1 0.001,0.04,0.04 /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_data_tensors/htn-tensor-subsetforanalysis-20140811-{0}.dat /nv/pcoc1/rchen87/tensor_factorization/github_tensor/experiment_code/ /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_expt_run/output_20140829/
+
+#python ../experiment_code/loadtensor_runfactorization_printphenotypes.py 50 1 0.001,0.06,0.06 /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_data_tensors/htn-tensor-subsetforanalysis-20140811-{0}.dat /nv/pcoc1/rchen87/tensor_factorization/github_tensor/experiment_code/ /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_expt_run/output_20140829/
+
+#python ../experiment_code/loadtensor_runfactorization_printphenotypes.py 50 1 0.001,0.08,0.08 /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_data_tensors/htn-tensor-subsetforanalysis-20140811-{0}.dat /nv/pcoc1/rchen87/tensor_factorization/github_tensor/experiment_code/ /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_expt_run/output_20140829/
+
+#python ../experiment_code/loadtensor_runfactorization_printphenotypes.py 50 1 0.001,0.09,0.09 /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_data_tensors/htn-tensor-subsetforanalysis-20140811-{0}.dat /nv/pcoc1/rchen87/tensor_factorization/github_tensor/experiment_code/ /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_expt_run/output_20140829/
+
+#python ../experiment_code/loadtensor_runfactorization_printphenotypes.py 50 1 0.001,0.11,0.11 /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_data_tensors/htn-tensor-subsetforanalysis-20140811-{0}.dat /nv/pcoc1/rchen87/tensor_factorization/github_tensor/experiment_code/ /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_expt_run/output_20140829/
+
+#python ../experiment_code/loadtensor_runfactorization_printphenotypes.py 50 1 0.001,0.12,0.12 /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_data_tensors/htn-tensor-subsetforanalysis-20140811-{0}.dat /nv/pcoc1/rchen87/tensor_factorization/github_tensor/experiment_code/ /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_expt_run/output_20140829/
+
+#python ../experiment_code/loadtensor_runfactorization_printphenotypes.py 50 1 0.001,0.13,0.13 /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_data_tensors/htn-tensor-subsetforanalysis-20140811-{0}.dat /nv/pcoc1/rchen87/tensor_factorization/github_tensor/experiment_code/ /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_expt_run/output_20140829/
+
+python ../experiment_code/loadtensor_runfactorization_printphenotypes.py 50 1 0.001,0.14,0.14 /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_data_tensors/htn-tensor-subsetforanalysis-20140811-{0}.dat /nv/pcoc1/rchen87/tensor_factorization/github_tensor/experiment_code/ /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_expt_run/output_20140829/
+
+python ../experiment_code/loadtensor_runfactorization_printphenotypes.py 50 1 0.001,0.15,0.15 /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_data_tensors/htn-tensor-subsetforanalysis-20140811-{0}.dat /nv/pcoc1/rchen87/tensor_factorization/github_tensor/experiment_code/ /nv/pcoc1/rchen87/tensor_factorization/github_tensor/htn_expt_run/output_20140829/
